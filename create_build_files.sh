ce1610c699035ed480f35dd873217489e8f6f690
Nicolas Macherey <nicolas@numero9.graymat>
2010-02-22 17:51:10 +0100

    create_build_files.sh has now options to let youchoose between codelite codeblock and makefiles

--- create_build_files.sh
+++ create_build_files.sh
@@ -15,19 +15,43 @@ if ! wx-config --debug >/dev/null 2>/dev/null; then
 	debug="--disable-wx-debug"
 fi
 
+plat="codelite"
+wxver=""
+
+for a in $*; do
+   	if [ $a = "codelite" ]; then
+   		plat="codelite"
+	fi
+	if [ $a = "codeblocks" ]; then
+   		plat="codeblocks"
+	fi
+	if [ $a = "makefile-gnu" ]; then
+   		plat="makefile-gnu"
+	fi
+	if [ $a = "--with-wx-28" ]; then
+   		wxver="--with-wx-28"
+	fi
+done
+
+if [ $plat = "codelite" ]; then
 #build codelite
-$PREMAKE_DIR/bin/premake --target cl-gcc $unicode $debug --with-wx-shared $1
+$PREMAKE_DIR/bin/premake --target cl-gcc $unicode $debug $wxver
 echo done...
 echo
+fi
 
+if [ $plat = "codeblocks" ]; then
 #build codeblocks
-#$PREMAKE_DIR/bin/premake --target cb-gcc $unicode $debug --with-wx-shared $1
-#echo done...
-#echo
-
-#build makefile
-$PREMAKE_DIR/bin/premake --target gnu $unicode $debug --with-wx-shared $1
+$PREMAKE_DIR/bin/premake --target cb-gcc $unicode $debug $wxver
 echo done...
 echo
+fi
+
+if [ $plat = "makefile-gnu" ]; then
+#build makefile
+$PREMAKE_DIR/bin/premake --target gnu $unicode $debug $wxver
+echo done...
+echo
+fi
 #
-echo Done generating all project files for wxFormBuilder.
+echo Done generating all project files for rheia.
