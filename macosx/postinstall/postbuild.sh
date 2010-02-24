204a3757cabf09404a61999c4f6f976afd21f5e5
Nicolas Macherey <nicolas@localforge.(none)>
2010-02-24 15:53:40 +0100

    Last changes for macos now bundles are properly generated thanx to this fucking ****** install_name_tool of mac which made simple things so complicated

--- macosx/postinstall/postbuild.sh
+++ macosx/postinstall/postbuild.sh
@@ -1,40 +1,113 @@
 #!/bin/sh
 
 LOCALDIR=../../devel/Release
-CONTENTS=$LOCALDIR/rheia_exe.app/Contents
+CONTENTS=$LOCALDIR/rheia.app/Contents
 BINDIR=$LOCALDIR/bin
 LIBDIR=$LOCALDIR/lib
 SHAREDIR=$LOCALDIR/share
 APPNAME=rheia
-APPNAME_EXE=rheia_exe
-APPNAME_EXED=rheia_exe.sh
 
-rm -rf ../../devel/Release/rheia_exe.app
+rm -rf ../../devel/Release/rheia.app
 
 mkdir -p $CONTENTS
 mkdir -p $CONTENTS/Resources
 mkdir -p $CONTENTS/MacOS
+mkdir -p $CONTENTS/lib
 mkdir -p $CONTENTS/Resources/share/$APPNAME
 
 cp -r $BINDIR/$APPNAME $CONTENTS/MacOS/$APPNAME
-cp -r $APPNAME_EXED $CONTENTS/MacOS/$APPNAME_EXE
-chmod a+x $CONTENTS/MacOS/$APPNAME_EXE
-cp -r $LIBDIR/* $CONTENTS/MacOS
+cp -r $LIBDIR/*.so.* $CONTENTS/lib
 cp -r $SHAREDIR/$APPNAME/* $CONTENTS/Resources/share/$APPNAME
 cp icon.icns $CONTENTS/Resources/icon.icns
 cp docicon.icns $CONTENTS/Resources/icon.icns
 cp Info.plist $CONTENTS/Info.plist
 
-#install_name_tool -id @executable_path/libgmirrlicht.so.1.6.1 $CONTENTS/MacOS/libgmirrlicht.so.1.6.1
-#install_name_tool -id @executable_path/librheiautils.so.1.1.1 $CONTENTS/MacOS/librheiautils.so.1.1.1
-#install_name_tool -id @executable_path/librheiabase.so.1.1.1 $CONTENTS/MacOS/librheiabase.so.1.1.1
-#install_name_tool -id @executable_path/librheialoggers.so.1.1.1 $CONTENTS/MacOS/librheialoggers.so.1.1.1
-#install_name_tool -id @executable_path/librheiapackagemgt.so.1.1.1 $CONTENTS/MacOS/librheiapackagemgt.so.1.1.1
-#install_name_tool -id @executable_path/librheiaworkspacemgt.so.1.1.1 $CONTENTS/MacOS/librheiaworkspacemgt.so.1.1.1
+install_name_tool -id @executable_path/../lib/libgmirrlicht.so.1.6.1 $CONTENTS/lib/libgmirrlicht.so.1.6.1
+install_name_tool -id @executable_path/../lib/librheiautils.so.1.1.1 $CONTENTS/lib/librheiautils.so.1.1.1
+install_name_tool -id @executable_path/../lib/librheiabase.so.1.1.1 $CONTENTS/lib/librheiabase.so.1.1.1
+install_name_tool -id @executable_path/../lib/librheialoggers.so.1.1.1 $CONTENTS/lib/librheialoggers.so.1.1.1
+install_name_tool -id @executable_path/../lib/librheiapackagemgt.so.1.1.1 $CONTENTS/lib/librheiapackagemgt.so.1.1.1
+install_name_tool -id @executable_path/../lib/librheiaworkspacemgt.so.1.1.1 $CONTENTS/lib/librheiaworkspacemgt.so.1.1.1
+install_name_tool -id @executable_path/../lib/libgmcsirocsa.so.1.1.1 $CONTENTS/lib/libgmcsirocsa.so.1.1.1
+install_name_tool -id @executable_path/../lib/libgmqsastime.so.1.1.1 $CONTENTS/lib/libgmqsastime.so.1.1.1
+install_name_tool -id @executable_path/../lib/libgmplplot.so.1.1.1 $CONTENTS/lib/libgmplplot.so.1.1.1
+install_name_tool -id @executable_path/../lib/libgmwxplplot.so.1.1.1 $CONTENTS/lib/libgmwxplplot.so.1.1.1
 
-#install_name_tool -change $LIBDIR/libgmirrlicht.so.1.6.1 @executable_path/libgmirrlicht.so.1.6.1 $CONTENTS/MacOS/libgmirrlicht.so.1.6.1
-#install_name_tool -change $LIBDIR/librheiautils.so.1.1.1 @executable_path/librheiautils.so.1.1.1 $CONTENTS/MacOS/librheiautils.so.1.1.1
-#install_name_tool -change $LIBDIR/librheiabase.so.1.1.1 @executable_path/librheiabase.so.1.1.1 $CONTENTS/MacOS/librheiabase.so.1.1.1
-#install_name_tool -change $LIBDIR/librheiloggers.so.1.1.1 @executable_path/librheialoggers.so.1.1.1 $CONTENTS/MacOS/librheialoggers.so.1.1.1
-#install_name_tool -change $LIBDIR/librheiapackagemgt.so.1.1.1 @executable_path/librheiapackagemgt.so.1.1.1 $CONTENTS/MacOS/librheiapackagemgt.so.1.1.1
-#install_name_tool -change $LIBDIR/librheiaworkspacemgt.so.1.1.1 @executable_path/librheiaworkspacemgt.so.1.1.1 $CONTENTS/MacOS/librheiaworkspacemgt.so.1.1.1
+#plplot 
+install_name_tool -change $LIBDIR/libgmcsirocsa.so.1.1.1 @executable_path/../lib/libgmcsirocsa.so.1.1.1 $CONTENTS/lib/libgmplplot.so.1.1.1
+install_name_tool -change $LIBDIR/libgmqsastime.so.1.1.1 @executable_path/../lib/libgmqsastime.so.1.1.1 $CONTENTS/lib/libgmplplot.so.1.1.1
+
+#wxplplot
+install_name_tool -change $LIBDIR/libgmcsirocsa.so.1.1.1 @executable_path/../lib/libgmcsirocsa.so.1.1.1 $CONTENTS/lib/libgmwxplplot.so.1.1.1
+install_name_tool -change $LIBDIR/libgmqsastime.so.1.1.1 @executable_path/../lib/libgmqsastime.so.1.1.1 $CONTENTS/lib/libgmwxplplot.so.1.1.1
+install_name_tool -change $LIBDIR/libgmplplot.so.1.1.1 @executable_path/../lib/libgmplplot.so.1.1.1 $CONTENTS/lib/libgmwxplplot.so.1.1.1
+
+#rheiautils
+install_name_tool -change $LIBDIR/libgmirrlicht.so.1.6.1 @executable_path/../lib/libgmirrlicht.so.1.6.1 $CONTENTS/lib/librheiautils.so.1.1.1
+install_name_tool -change $LIBDIR/libgmcsirocsa.so.1.1.1 @executable_path/../lib/libgmcsirocsa.so.1.1.1 $CONTENTS/lib/librheiautils.so.1.1.1
+install_name_tool -change $LIBDIR/libgmqsastime.so.1.1.1 @executable_path/../lib/libgmqsastime.so.1.1.1 $CONTENTS/lib/librheiautils.so.1.1.1
+install_name_tool -change $LIBDIR/libgmplplot.so.1.1.1 @executable_path/../lib/libgmplplot.so.1.1.1 $CONTENTS/lib/librheiautils.so.1.1.1
+install_name_tool -change $LIBDIR/libgmwxplplot.so.1.1.1 @executable_path/../lib/libgmwxplplot.so.1.1.1 $CONTENTS/lib/librheiautils.so.1.1.1
+
+#rheiabase
+install_name_tool -change $LIBDIR/libgmirrlicht.so.1.6.1 @executable_path/../lib/libgmirrlicht.so.1.6.1 $CONTENTS/lib/librheiabase.so.1.1.1
+install_name_tool -change $LIBDIR/libgmcsirocsa.so.1.1.1 @executable_path/../lib/libgmcsirocsa.so.1.1.1 $CONTENTS/lib/librheiabase.so.1.1.1
+install_name_tool -change $LIBDIR/libgmqsastime.so.1.1.1 @executable_path/../lib/libgmqsastime.so.1.1.1 $CONTENTS/lib/librheiabase.so.1.1.1
+install_name_tool -change $LIBDIR/libgmplplot.so.1.1.1 @executable_path/../lib/libgmplplot.so.1.1.1 $CONTENTS/lib/librheiabase.so.1.1.1
+install_name_tool -change $LIBDIR/libgmwxplplot.so.1.1.1 @executable_path/../lib/libgmwxplplot.so.1.1.1 $CONTENTS/lib/librheiabase.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiautils.so.1.1.1 @executable_path/../lib/librheiautils.so.1.1.1 $CONTENTS/lib/librheiabase.so.1.1.1
+
+#rheialoggers
+install_name_tool -change $LIBDIR/libgmirrlicht.so.1.6.1 @executable_path/../lib/libgmirrlicht.so.1.6.1 $CONTENTS/lib/librheialoggers.so.1.1.1
+install_name_tool -change $LIBDIR/libgmcsirocsa.so.1.1.1 @executable_path/../lib/libgmcsirocsa.so.1.1.1 $CONTENTS/lib/librheialoggers.so.1.1.1
+install_name_tool -change $LIBDIR/libgmqsastime.so.1.1.1 @executable_path/../lib/libgmqsastime.so.1.1.1 $CONTENTS/lib/librheialoggers.so.1.1.1
+install_name_tool -change $LIBDIR/libgmplplot.so.1.1.1 @executable_path/../lib/libgmplplot.so.1.1.1 $CONTENTS/lib/librheialoggers.so.1.1.1
+install_name_tool -change $LIBDIR/libgmwxplplot.so.1.1.1 @executable_path/../lib/libgmwxplplot.so.1.1.1 $CONTENTS/lib/librheialoggers.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiautils.so.1.1.1 @executable_path/../lib/librheiautils.so.1.1.1 $CONTENTS/lib/librheialoggers.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiabase.so.1.1.1 @executable_path/../lib/librheiabase.so.1.1.1 $CONTENTS/lib/librheialoggers.so.1.1.1
+
+#rheiapackagemgt
+install_name_tool -change $LIBDIR/libgmirrlicht.so.1.6.1 @executable_path/../lib/libgmirrlicht.so.1.6.1 $CONTENTS/lib/librheiapackagemgt.so.1.1.1
+install_name_tool -change $LIBDIR/libgmcsirocsa.so.1.1.1 @executable_path/../lib/libgmcsirocsa.so.1.1.1 $CONTENTS/lib/librheiapackagemgt.so.1.1.1
+install_name_tool -change $LIBDIR/libgmqsastime.so.1.1.1 @executable_path/../lib/libgmqsastime.so.1.1.1 $CONTENTS/lib/librheiapackagemgt.so.1.1.1
+install_name_tool -change $LIBDIR/libgmplplot.so.1.1.1 @executable_path/../lib/libgmplplot.so.1.1.1 $CONTENTS/lib/librheiapackagemgt.so.1.1.1
+install_name_tool -change $LIBDIR/libgmwxplplot.so.1.1.1 @executable_path/../lib/libgmwxplplot.so.1.1.1 $CONTENTS/lib/librheiapackagemgt.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiautils.so.1.1.1 @executable_path/../lib/librheiautils.so.1.1.1 $CONTENTS/lib/librheiapackagemgt.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiabase.so.1.1.1 @executable_path/../lib/librheiabase.so.1.1.1 $CONTENTS/lib/librheiapackagemgt.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheialoggers.so.1.1.1 @executable_path/../lib/librheialoggers.so.1.1.1 $CONTENTS/lib/librheiapackagemgt.so.1.1.1
+
+#rheiaworkspacemgt
+install_name_tool -change $LIBDIR/libgmirrlicht.so.1.6.1 @executable_path/../lib/libgmirrlicht.so.1.6.1 $CONTENTS/lib/librheiaworkspacemgt.so.1.1.1
+install_name_tool -change $LIBDIR/libgmcsirocsa.so.1.1.1 @executable_path/../lib/libgmcsirocsa.so.1.1.1 $CONTENTS/lib/librheiaworkspacemgt.so.1.1.1
+install_name_tool -change $LIBDIR/libgmqsastime.so.1.1.1 @executable_path/../lib/libgmqsastime.so.1.1.1 $CONTENTS/lib/librheiaworkspacemgt.so.1.1.1
+install_name_tool -change $LIBDIR/libgmplplot.so.1.1.1 @executable_path/../lib/libgmplplot.so.1.1.1 $CONTENTS/lib/librheiaworkspacemgt.so.1.1.1
+install_name_tool -change $LIBDIR/libgmwxplplot.so.1.1.1 @executable_path/../lib/libgmwxplplot.so.1.1.1 $CONTENTS/lib/librheiaworkspacemgt.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiautils.so.1.1.1 @executable_path/../lib/librheiautils.so.1.1.1 $CONTENTS/lib/librheiaworkspacemgt.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiabase.so.1.1.1 @executable_path/../lib/librheiabase.so.1.1.1 $CONTENTS/lib/librheiaworkspacemgt.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheialoggers.so.1.1.1 @executable_path/../lib/librheialoggers.so.1.1.1 $CONTENTS/lib/librheiaworkspacemgt.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiapackagemgt.so.1.1.1 @executable_path/../lib/librheiapackagemgt.so.1.1.1 $CONTENTS/lib/librheiaworkspacemgt.so.1.1.1
+
+#rheiapython
+install_name_tool -change $LIBDIR/libgmirrlicht.so.1.6.1 @executable_path/../lib/libgmirrlicht.so.1.6.1 $CONTENTS/lib/librheiapython.so.1.1.1
+install_name_tool -change $LIBDIR/libgmcsirocsa.so.1.1.1 @executable_path/../lib/libgmcsirocsa.so.1.1.1 $CONTENTS/lib/librheiapython.so.1.1.1
+install_name_tool -change $LIBDIR/libgmqsastime.so.1.1.1 @executable_path/../lib/libgmqsastime.so.1.1.1 $CONTENTS/lib/librheiapython.so.1.1.1
+install_name_tool -change $LIBDIR/libgmplplot.so.1.1.1 @executable_path/../lib/libgmplplot.so.1.1.1 $CONTENTS/lib/librheiapython.so.1.1.1
+install_name_tool -change $LIBDIR/libgmwxplplot.so.1.1.1 @executable_path/../lib/libgmwxplplot.so.1.1.1 $CONTENTS/lib/librheiapython.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiautils.so.1.1.1 @executable_path/../lib/librheiautils.so.1.1.1 $CONTENTS/lib/librheiapython.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiabase.so.1.1.1 @executable_path/../lib/librheiabase.so.1.1.1 $CONTENTS/lib/librheiapython.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheialoggers.so.1.1.1 @executable_path/../lib/librheialoggers.so.1.1.1 $CONTENTS/lib/librheiapython.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiapackagemgt.so.1.1.1 @executable_path/../lib/librheiapackagemgt.so.1.1.1 $CONTENTS/lib/librheiapython.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiaworkspacemgt.so.1.1.1 @executable_path/../lib/librheiaworkspacemgt.so.1.1.1 $CONTENTS/lib/librheiapython.so.1.1.1
+
+#rheia
+install_name_tool -change $LIBDIR/libgmirrlicht.so.1.6.1 @executable_path/../lib/libgmirrlicht.so.1.6.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change ../$LIBDIR/librheiautils.so.1.1.1 @executable_path/../lib/librheiautils.so.1.1.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change ../$LIBDIR/librheiabase.so.1.1.1 @executable_path/../lib/librheiabase.so.1.1.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change ../$LIBDIR/librheiapython.so.1.1.1 @executable_path/../lib/librheiapython.so.1.1.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change ../$LIBDIR/librheialoggers.so.1.1.1 @executable_path/../lib/librheialoggers.so.1.1.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change ../$LIBDIR/librheiapackagemgt.so.1.1.1 @executable_path/../lib/librheiapackagemgt.so.1.1.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change ../$LIBDIR/librheiaworkspacemgt.so.1.1.1 @executable_path/../lib/librheiaworkspacemgt.so.1.1.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change $LIBDIR/libgmcsirocsa.so.1.1.1 @executable_path/../lib/libgmcsirocsa.so.1.1.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change $LIBDIR/libgmqsastime.so.1.1.1 @executable_path/../lib/libgmqsastime.so.1.1.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change $LIBDIR/libgmplplot.so.1.1.1 @executable_path/../lib/libgmplplot.so.1.1.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change $LIBDIR/libgmwxplplot.so.1.1.1 @executable_path/../lib/libgmwxplplot.so.1.1.1 $CONTENTS/MacOS/$APPNAME
--- macosx/postinstall/pyedt_postbuildd.sh
+++ macosx/postinstall/pyedt_postbuildd.sh
@@ -1,40 +1,113 @@
 #!/bin/sh
 
-LOCALDIR=../../devel/Release
-CONTENTS=$LOCALDIR/pyedt_exe.app/Contents
+LOCALDIR=../../devel/Debug
+CONTENTS=$LOCALDIR/pyedt.app/Contents
 BINDIR=$LOCALDIR/bin
 LIBDIR=$LOCALDIR/lib
 SHAREDIR=$LOCALDIR/share
 APPNAME=pyedt
-APPNAME_EXE=pyedt_exe
-APPNAME_EXED=pyedt_exe.sh
 
-rm -rf ../../devel/Release/pyedt_exe.app
+rm -rf ../../devel/Debud/pyedt.app
 
 mkdir -p $CONTENTS
 mkdir -p $CONTENTS/Resources
 mkdir -p $CONTENTS/MacOS
+mkdir -p $CONTENTS/lib
 mkdir -p $CONTENTS/Resources/share/$APPNAME
 
-cp -r $BINDIR/$APPNAME $CONTENTS/MacOS/$APPNAME
-cp -r $APPNAME_EXED $CONTENTS/MacOS/$APPNAME_EXE
-chmod a+x $CONTENTS/MacOS/$APPNAME_EXE
-cp -r $LIBDIR/* $CONTENTS/MacOS
+cp -r $BINDIR/pyedt-dbg $CONTENTS/MacOS/$APPNAME
+cp -r $LIBDIR/*-dbg.so.* $CONTENTS/lib
 cp -r $SHAREDIR/$APPNAME/* $CONTENTS/Resources/share/$APPNAME
 cp icon.icns $CONTENTS/Resources/icon.icns
 cp docicon.icns $CONTENTS/Resources/icon.icns
 cp pyedtInfo.plist $CONTENTS/Info.plist
 
-#install_name_tool -id @executable_path/libgmirrlicht.so.1.6.1 $CONTENTS/MacOS/libgmirrlicht.so.1.6.1
-#install_name_tool -id @executable_path/librheiautils.so.1.1.1 $CONTENTS/MacOS/librheiautils.so.1.1.1
-#install_name_tool -id @executable_path/librheiabase.so.1.1.1 $CONTENTS/MacOS/librheiabase.so.1.1.1
-#install_name_tool -id @executable_path/librheialoggers.so.1.1.1 $CONTENTS/MacOS/librheialoggers.so.1.1.1
-#install_name_tool -id @executable_path/librheiapackagemgt.so.1.1.1 $CONTENTS/MacOS/librheiapackagemgt.so.1.1.1
-#install_name_tool -id @executable_path/librheiaworkspacemgt.so.1.1.1 $CONTENTS/MacOS/librheiaworkspacemgt.so.1.1.1
+install_name_tool -id @executable_path/../lib/libgmirrlicht-dbg.so.1.6.1 $CONTENTS/lib/libgmirrlicht-dbg.so.1.6.1
+install_name_tool -id @executable_path/../lib/librheiautils-dbg.so.1.1.1 $CONTENTS/lib/librheiautils-dbg.so.1.1.1
+install_name_tool -id @executable_path/../lib/librheiabase-dbg.so.1.1.1 $CONTENTS/lib/librheiabase-dbg.so.1.1.1
+install_name_tool -id @executable_path/../lib/librheialoggers-dbg.so.1.1.1 $CONTENTS/lib/librheialoggers-dbg.so.1.1.1
+install_name_tool -id @executable_path/../lib/librheiapackagemgt-dbg.so.1.1.1 $CONTENTS/lib/librheiapackagemgt-dbg.so.1.1.1
+install_name_tool -id @executable_path/../lib/librheiaworkspacemgt-dbg.so.1.1.1 $CONTENTS/lib/librheiaworkspacemgt-dbg.so.1.1.1
+install_name_tool -id @executable_path/../lib/libgmcsirocsa-dbg.so.1.1.1 $CONTENTS/lib/libgmcsirocsa-dbg.so.1.1.1
+install_name_tool -id @executable_path/../lib/libgmqsastime-dbg.so.1.1.1 $CONTENTS/lib/libgmqsastime-dbg.so.1.1.1
+install_name_tool -id @executable_path/../lib/libgmplplot-dbg.so.1.1.1 $CONTENTS/lib/libgmplplot-dbg.so.1.1.1
+install_name_tool -id @executable_path/../lib/libgmwxplplot-dbg.so.1.1.1 $CONTENTS/lib/libgmwxplplot-dbg.so.1.1.1
 
-#install_name_tool -change $LIBDIR/libgmirrlicht.so.1.6.1 @executable_path/libgmirrlicht.so.1.6.1 $CONTENTS/MacOS/libgmirrlicht.so.1.6.1
-#install_name_tool -change $LIBDIR/librheiautils.so.1.1.1 @executable_path/librheiautils.so.1.1.1 $CONTENTS/MacOS/librheiautils.so.1.1.1
-#install_name_tool -change $LIBDIR/librheiabase.so.1.1.1 @executable_path/librheiabase.so.1.1.1 $CONTENTS/MacOS/librheiabase.so.1.1.1
-#install_name_tool -change $LIBDIR/librheiloggers.so.1.1.1 @executable_path/librheialoggers.so.1.1.1 $CONTENTS/MacOS/librheialoggers.so.1.1.1
-#install_name_tool -change $LIBDIR/librheiapackagemgt.so.1.1.1 @executable_path/librheiapackagemgt.so.1.1.1 $CONTENTS/MacOS/librheiapackagemgt.so.1.1.1
-#install_name_tool -change $LIBDIR/librheiaworkspacemgt.so.1.1.1 @executable_path/librheiaworkspacemgt.so.1.1.1 $CONTENTS/MacOS/librheiaworkspacemgt.so.1.1.1
+#plplot 
+install_name_tool -change $LIBDIR/libgmcsirocsa-dbg.so.1.1.1 @executable_path/../lib/libgmcsirocsa-dbg.so.1.1.1 $CONTENTS/lib/libgmplplot-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmqsastime-dbg.so.1.1.1 @executable_path/../lib/libgmqsastime-dbg.so.1.1.1 $CONTENTS/lib/libgmplplot-dbg.so.1.1.1
+
+#wxplplot
+install_name_tool -change $LIBDIR/libgmcsirocsa-dbg.so.1.1.1 @executable_path/../lib/libgmcsirocsa-dbg.so.1.1.1 $CONTENTS/lib/libgmwxplplot-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmqsastime-dbg.so.1.1.1 @executable_path/../lib/libgmqsastime-dbg.so.1.1.1 $CONTENTS/lib/libgmwxplplot-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmplplot-dbg.so.1.1.1 @executable_path/../lib/libgmplplot-dbg.so.1.1.1 $CONTENTS/lib/libgmwxplplot-dbg.so.1.1.1
+
+#rheiautils
+install_name_tool -change $LIBDIR/libgmirrlicht-dbg.so.1.6.1 @executable_path/../lib/libgmirrlicht-dbg.so.1.6.1 $CONTENTS/lib/librheiautils-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmcsirocsa-dbg.so.1.1.1 @executable_path/../lib/libgmcsirocsa-dbg.so.1.1.1 $CONTENTS/lib/librheiautils-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmqsastime-dbg.so.1.1.1 @executable_path/../lib/libgmqsastime-dbg.so.1.1.1 $CONTENTS/lib/librheiautils-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmplplot-dbg.so.1.1.1 @executable_path/../lib/libgmplplot-dbg.so.1.1.1 $CONTENTS/lib/librheiautils-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmwxplplot-dbg.so.1.1.1 @executable_path/../lib/libgmwxplplot-dbg.so.1.1.1 $CONTENTS/lib/librheiautils-dbg.so.1.1.1
+
+#rheiabase
+install_name_tool -change $LIBDIR/libgmirrlicht-dbg.so.1.6.1 @executable_path/../lib/libgmirrlicht-dbg.so.1.6.1 $CONTENTS/lib/librheiabase-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmcsirocsa-dbg.so.1.1.1 @executable_path/../lib/libgmcsirocsa-dbg.so.1.1.1 $CONTENTS/lib/librheiabase-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmqsastime-dbg.so.1.1.1 @executable_path/../lib/libgmqsastime-dbg.so.1.1.1 $CONTENTS/lib/librheiabase-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmplplot-dbg.so.1.1.1 @executable_path/../lib/libgmplplot-dbg.so.1.1.1 $CONTENTS/lib/librheiabase-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmwxplplot-dbg.so.1.1.1 @executable_path/../lib/libgmwxplplot-dbg.so.1.1.1 $CONTENTS/lib/librheiabase-dbg.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiautils-dbg.so.1.1.1 @executable_path/../lib/librheiautils-dbg.so.1.1.1 $CONTENTS/lib/librheiabase-dbg.so.1.1.1
+
+#rheialoggers
+install_name_tool -change $LIBDIR/libgmirrlicht-dbg.so.1.6.1 @executable_path/../lib/libgmirrlicht-dbg.so.1.6.1 $CONTENTS/lib/librheialoggers-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmcsirocsa-dbg.so.1.1.1 @executable_path/../lib/libgmcsirocsa-dbg.so.1.1.1 $CONTENTS/lib/librheialoggers-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmqsastime-dbg.so.1.1.1 @executable_path/../lib/libgmqsastime-dbg.so.1.1.1 $CONTENTS/lib/librheialoggers-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmplplot-dbg.so.1.1.1 @executable_path/../lib/libgmplplot-dbg.so.1.1.1 $CONTENTS/lib/librheialoggers-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmwxplplot-dbg.so.1.1.1 @executable_path/../lib/libgmwxplplot-dbg.so.1.1.1 $CONTENTS/lib/librheialoggers-dbg.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiautils-dbg.so.1.1.1 @executable_path/../lib/librheiautils-dbg.so.1.1.1 $CONTENTS/lib/librheialoggers-dbg.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiabase-dbg.so.1.1.1 @executable_path/../lib/librheiabase-dbg.so.1.1.1 $CONTENTS/lib/librheialoggers-dbg.so.1.1.1
+
+#rheiapackagemgt
+install_name_tool -change $LIBDIR/libgmirrlicht-dbg.so.1.6.1 @executable_path/../lib/libgmirrlicht-dbg.so.1.6.1 $CONTENTS/lib/librheiapackagemgt-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmcsirocsa-dbg.so.1.1.1 @executable_path/../lib/libgmcsirocsa-dbg.so.1.1.1 $CONTENTS/lib/librheiapackagemgt-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmqsastime-dbg.so.1.1.1 @executable_path/../lib/libgmqsastime-dbg.so.1.1.1 $CONTENTS/lib/librheiapackagemgt-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmplplot-dbg.so.1.1.1 @executable_path/../lib/libgmplplot-dbg.so.1.1.1 $CONTENTS/lib/librheiapackagemgt-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmwxplplot-dbg.so.1.1.1 @executable_path/../lib/libgmwxplplot-dbg.so.1.1.1 $CONTENTS/lib/librheiapackagemgt-dbg.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiautils-dbg.so.1.1.1 @executable_path/../lib/librheiautils-dbg.so.1.1.1 $CONTENTS/lib/librheiapackagemgt-dbg.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiabase-dbg.so.1.1.1 @executable_path/../lib/librheiabase-dbg.so.1.1.1 $CONTENTS/lib/librheiapackagemgt-dbg.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheialoggers-dbg.so.1.1.1 @executable_path/../lib/librheialoggers-dbg.so.1.1.1 $CONTENTS/lib/librheiapackagemgt-dbg.so.1.1.1
+
+#rheiaworkspacemgt
+install_name_tool -change $LIBDIR/libgmirrlicht-dbg.so.1.6.1 @executable_path/../lib/libgmirrlicht-dbg.so.1.6.1 $CONTENTS/lib/librheiaworkspacemgt-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmcsirocsa-dbg.so.1.1.1 @executable_path/../lib/libgmcsirocsa-dbg.so.1.1.1 $CONTENTS/lib/librheiaworkspacemgt-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmqsastime-dbg.so.1.1.1 @executable_path/../lib/libgmqsastime-dbg.so.1.1.1 $CONTENTS/lib/librheiaworkspacemgt-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmplplot-dbg.so.1.1.1 @executable_path/../lib/libgmplplot-dbg.so.1.1.1 $CONTENTS/lib/librheiaworkspacemgt-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmwxplplot-dbg.so.1.1.1 @executable_path/../lib/libgmwxplplot-dbg.so.1.1.1 $CONTENTS/lib/librheiaworkspacemgt-dbg.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiautils-dbg.so.1.1.1 @executable_path/../lib/librheiautils-dbg.so.1.1.1 $CONTENTS/lib/librheiaworkspacemgt-dbg.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiabase-dbg.so.1.1.1 @executable_path/../lib/librheiabase-dbg.so.1.1.1 $CONTENTS/lib/librheiaworkspacemgt-dbg.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheialoggers-dbg.so.1.1.1 @executable_path/../lib/librheialoggers-dbg.so.1.1.1 $CONTENTS/lib/librheiaworkspacemgt-dbg.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiapackagemgt-dbg.so.1.1.1 @executable_path/../lib/librheiapackagemgt-dbg.so.1.1.1 $CONTENTS/lib/librheiaworkspacemgt-dbg.so.1.1.1
+
+#rheiapython
+install_name_tool -change $LIBDIR/libgmirrlicht-dbg.so.1.6.1 @executable_path/../lib/libgmirrlicht-dbg.so.1.6.1 $CONTENTS/lib/librheiapython-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmcsirocsa-dbg.so.1.1.1 @executable_path/../lib/libgmcsirocsa-dbg.so.1.1.1 $CONTENTS/lib/librheiapython-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmqsastime-dbg.so.1.1.1 @executable_path/../lib/libgmqsastime-dbg.so.1.1.1 $CONTENTS/lib/librheiapython-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmplplot-dbg.so.1.1.1 @executable_path/../lib/libgmplplot-dbg.so.1.1.1 $CONTENTS/lib/librheiapython-dbg.so.1.1.1
+install_name_tool -change $LIBDIR/libgmwxplplot-dbg.so.1.1.1 @executable_path/../lib/libgmwxplplot-dbg.so.1.1.1 $CONTENTS/lib/librheiapython-dbg.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiautils-dbg.so.1.1.1 @executable_path/../lib/librheiautils-dbg.so.1.1.1 $CONTENTS/lib/librheiapython-dbg.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiabase-dbg.so.1.1.1 @executable_path/../lib/librheiabase-dbg.so.1.1.1 $CONTENTS/lib/librheiapython-dbg.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheialoggers-dbg.so.1.1.1 @executable_path/../lib/librheialoggers-dbg.so.1.1.1 $CONTENTS/lib/librheiapython-dbg.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiapackagemgt-dbg.so.1.1.1 @executable_path/../lib/librheiapackagemgt-dbg.so.1.1.1 $CONTENTS/lib/librheiapython-dbg.so.1.1.1
+install_name_tool -change ../$LIBDIR/librheiaworkspacemgt-dbg.so.1.1.1 @executable_path/../lib/librheiaworkspacemgt-dbg.so.1.1.1 $CONTENTS/lib/librheiapython-dbg.so.1.1.1
+
+#rheia
+install_name_tool -change $LIBDIR/libgmirrlicht-dbg.so.1.6.1 @executable_path/../lib/libgmirrlicht-dbg.so.1.6.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change ../$LIBDIR/librheiautils-dbg.so.1.1.1 @executable_path/../lib/librheiautils-dbg.so.1.1.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change ../$LIBDIR/librheiabase-dbg.so.1.1.1 @executable_path/../lib/librheiabase-dbg.so.1.1.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change ../$LIBDIR/librheiapython-dbg.so.1.1.1 @executable_path/../lib/librheiapython-dbg.so.1.1.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change ../$LIBDIR/librheialoggers-dbg.so.1.1.1 @executable_path/../lib/librheialoggers-dbg.so.1.1.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change ../$LIBDIR/librheiapackagemgt-dbg.so.1.1.1 @executable_path/../lib/librheiapackagemgt-dbg.so.1.1.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change ../$LIBDIR/librheiaworkspacemgt-dbg.so.1.1.1 @executable_path/../lib/librheiaworkspacemgt-dbg.so.1.1.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change $LIBDIR/libgmcsirocsa-dbg.so.1.1.1 @executable_path/../lib/libgmcsirocsa-dbg.so.1.1.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change $LIBDIR/libgmqsastime-dbg.so.1.1.1 @executable_path/../lib/libgmqsastime-dbg.so.1.1.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change $LIBDIR/libgmplplot-dbg.so.1.1.1 @executable_path/../lib/libgmplplot-dbg.so.1.1.1 $CONTENTS/MacOS/$APPNAME
+install_name_tool -change $LIBDIR/libgmwxplplot-dbg.so.1.1.1 @executable_path/../lib/libgmwxplplot-dbg.so.1.1.1 $CONTENTS/MacOS/$APPNAME
